4b758a48a945fd266084adcae54e5438bbf1b9de
Rastko K <63473032+notrastko@users.noreply.github.com>
2024-03-26 20:37:00 -0700

    MVP

---
+++ script.js
@@ -0,0 +1,36 @@
+async function search() {
+    const username = document.getElementById("usernameInput").value;
+    if (!username) {
+        alert("Please enter a username");
+        return;
+    }
+
+    const response = await fetch("https://api.jsonbin.io/v3/qs/660392622b1b334a633bf5b6");
+    const responseData = await response.json();
+
+    // Extract the record array from the response data
+    const data = responseData.record;
+
+    if (!Array.isArray(data)) {
+        console.error("Data is not an array:", data);
+        return;
+    }
+
+    const result = data.filter(record => record.fields.username === username);
+    if (result.length > 0) {
+        const banRecord = result[0].fields;
+        const formattedBanDate = new Date(banRecord.ban_date).toLocaleString();
+
+        document.getElementById("result").innerHTML = `
+            <strong>Username:</strong> ${banRecord.username}<br>
+            <strong>User ID:</strong> ${banRecord.user_id}<br>
+            <strong>Ban Date:</strong> ${formattedBanDate}<br>
+            <strong>Ban Reason:</strong> ${banRecord.reason}<br>
+            <strong>Banned by Mod:</strong> ${banRecord.mod}
+        `;
+    } else {
+        document.getElementById("result").innerHTML = "No ban record found for the given username.";
+    }
+}
+
+
